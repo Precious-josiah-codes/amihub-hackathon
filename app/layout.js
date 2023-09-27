be7a69bfd31e26fc9b7bf7599e5ab8188a39b5ab
chukusuccess <successchuku5@gmail.com>
2023-09-27 22:28:05 +0100

    mend

--- app/layout.js
+++ app/layout.js
@@ -145,7 +145,7 @@ export default function RootLayout({ children }) {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-NWJXNWWPTZ"
       ></Script>
-      <Script strategy="afterInteractive">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || []; function gtag()
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'G-NWJXNWWPTZ');`}

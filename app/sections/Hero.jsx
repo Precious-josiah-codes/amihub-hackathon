6f36c9753088a2a516dfd15c16dffd05739388bb
chukusuccess <successchuku5@gmail.com>
2023-10-14 12:57:12 +0100

    masked redirection with a spinner

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -4,8 +4,25 @@ import { content } from "../data/content";
 import { motion } from "framer-motion";
 import { animationOne, animationThree, animationTwo } from "../animation";
 
+const redirecting = true;
+
 const Hero = () => {
   const { hero } = content;
+
+  if (redirecting) {
+    return (
+      <div class="flex flex-col items-center justify-center sm:min-h-screen h-[85vh] bg-primaryColor">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <section class="flex flex-col items-center justify-center sm:min-h-screen h-[85vh]">
